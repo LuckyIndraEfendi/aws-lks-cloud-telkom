f56eff1cd555680f0021edf2bc2b87a8dd6b1e2b
Lucky Indra Efendi <kimilbonchu@gmail.com>
2023-08-27 19:26:44 +0700

    update code

---
+++ client/src/components/CardContent.jsx
@@ -0,0 +1,142 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+import { Typography, Button, Box } from '@mui/material';
+import axios from 'axios';
+
+export default function TitlebarImageList() {
+    const [comic, setComic] = React.useState([]);
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState(null);
+    const [ip, setIp] = React.useState([]);
+
+    const getIPAddress = async () => {
+        try {
+            const response = await axios.get('https://api.db-ip.com/v2/free/self');
+            const data = await response.data;
+            setIp(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    React.useEffect(() => {
+        getIPAddress()
+    }, [])
+    return (
+        <ImageList sx={{ width: "90%", margin: "50px auto" }} gap={10}>
+            <ImageListItem key="Subheader" cols={4}>
+                <ListSubheader component="div" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <Box>
+                        <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: "20px" }}>Your IP Address : {ip.ipAddress}</Typography>
+                        <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: "20px" }}>Country Name : {ip.countryName}</Typography>
+                    </Box>
+                    <Button variant="contained" href="#contained-buttons" color="success">
+                        Add Comic
+                    </Button>
+
+                </ListSubheader>
+            </ImageListItem>
+            {itemData.map((item) => (
+                <ImageListItem key={item.img} sx={{ borderRadius: "5px", overflow: "hidden", }}>
+                    <img
+                        src={`${item.img}?w=248&fit=crop&auto=format`}
+                        srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                        alt={item.title}
+                        loading="lazy"
+
+                    />
+                    <ImageListItemBar
+                        title={item.title}
+                        subtitle={item.author}
+                        actionIcon={
+                            <IconButton
+                                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                aria-label={`info about ${item.title}`}
+                            >
+                                <InfoIcon />
+                            </IconButton>
+                        }
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
+}
+
+const itemData = [
+    {
+        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        title: 'Breakfast',
+        author: '@bkristastucchio',
+        rows: 2,
+        cols: 2,
+        featured: true,
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        title: 'Burger',
+        author: '@rollelflex_graphy726',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+        author: '@helloimnik',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        title: 'Coffee',
+        author: '@nolanissac',
+        cols: 2,
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        title: 'Hats',
+        author: '@hjrc33',
+        cols: 2,
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+        title: 'Honey',
+        author: '@arwinneil',
+        rows: 2,
+        cols: 2,
+        featured: true,
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+        title: 'Basketball',
+        author: '@tjdragotta',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+        title: 'Fern',
+        author: '@katie_wasserman',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+        title: 'Mushrooms',
+        author: '@silverdalex',
+        rows: 2,
+        cols: 2,
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+        title: 'Tomato basil',
+        author: '@shelleypauls',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+        title: 'Sea star',
+        author: '@peterlaster',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+        title: 'Bike',
+        author: '@southside_customs',
+        cols: 2,
+    },
+];
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import CardContent from '../components/CardContent'
+
+const Dashboard = () => {
+    return (
+        <>
+            <Navbar />
+            <CardContent />
+        </>
+    )
+}
+
+export default Dashboard
